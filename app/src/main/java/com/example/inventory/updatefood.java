b5a8a7325d33bdabd738c16de6d86752d807dc48
minosh <it20088682@my.sliit.lk>
2021-08-13 00:13:21 +0530

    commit

---
+++ app/src/main/java/com/example/inventory/updatefood.java
@@ -0,0 +1,13 @@
+package com.example.inventory;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class updatefood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updatefood);
+    }
+}
